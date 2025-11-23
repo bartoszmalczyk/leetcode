9f55dd27dfbcaf8b454805228cbbd6616eb41be5
Bartosz Malczyk <142089705+bmalczan@users.noreply.github.com>
2025-11-23 10:32:52 +0100

    Time: 11 ms (91.42%), Space: 21.9 MB (45.27%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -1,22 +1,25 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
         total = sum(nums)
-        remainder = total % 3
-        if remainder == 0:
-            return total
-        mod1 = sorted([x for x in nums if x % 3 == 1])
-        mod2 = sorted([x for x in nums if x % 3 == 2])
-        to_remove = float('inf')
+        if total % 3 == 0:
+            return total 
+        else:
+            mod1 = sorted([x for x in nums if x % 3 == 1])
+            mod2 = sorted([x for x in nums if x % 3 == 2])
+            reminder = total % 3 
+            
+            to_remove = 1001
+            if reminder == 1:
+                if mod1: 
+                    to_remove = min(mod1[0], to_remove)
+                if len(mod2) >= 2:
+                    to_remove = min(mod2[0] + mod2[1], to_remove)
+            else:
+                if mod2: 
+                    to_remove = min(mod2[0], to_remove)
+                if len(mod1) >= 2:
+                    to_remove = min(mod1[0] + mod1[1], to_remove)
+            return total - to_remove
 
-        if remainder == 1:
-            if mod1:
-                to_remove = min(to_remove, mod1[0])
-            if len(rem2) >= 2:
-                to_remove = min(to_remove, mod2[0] + mod2[1])
+                
 
-        elif remainder == 2:
-            if mod2:
-                to_remove = min(to_remove, mod2[0])
-            if len(mod1) >= 2:
-                to_remove = min(to_remove, mod1[0] + mod1[1])
-        return total - to_remove
