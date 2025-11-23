172e9870f0b90dd59a6dd1c7d020871d67485874
Bartosz Malczyk <142089705+bmalczan@users.noreply.github.com>
2025-11-23 10:24:17 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -4,19 +4,19 @@ class Solution:
         remainder = total % 3
         if remainder == 0:
             return total
-        rem1 = sorted([x for x in nums if x % 3 == 1])
-        rem2 = sorted([x for x in nums if x % 3 == 2])
+        mod1 = sorted([x for x in nums if x % 3 == 1])
+        mod2 = sorted([x for x in nums if x % 3 == 2])
         to_remove = float('inf')
-        if remainder == 1:
-            if rem1:
-                to_remove = min(to_remove, rem1[0])
-            if len(rem2) >= 2:
-                to_remove = min(to_remove, rem2[0] + rem2[1])
-                
-        elif remainder == 2:
-            if rem2:
-                to_remove = min(to_remove, rem2[0])
-            if len(rem1) >= 2:
-                to_remove = min(to_remove, rem1[0] + rem1[1])
 
+        if remainder == 1:
+            if mod1:
+                to_remove = min(to_remove, mod1[0])
+            if len(rem2) >= 2:
+                to_remove = min(to_remove, mod2[0] + mod2[1])
+
+        elif remainder == 2:
+            if mod2:
+                to_remove = min(to_remove, mod2[0])
+            if len(mod1) >= 2:
+                to_remove = min(to_remove, mod1[0] + mod1[1])
         return total - to_remove
