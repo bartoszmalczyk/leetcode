0dd1717356cc30f6cc445534ce966efad28ae9f6
Bartosz Malczyk <142089705+bmalczan@users.noreply.github.com>
2025-12-08 15:58:52 +0100

    Time: 212 ms (49.46%), Space: 18.1 MB (11.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -7,7 +7,7 @@ class Solution:
             for j in range(1, n + 1):
                 val = i ** 2 + j ** 2
                 c_sqrt = math.sqrt(val)
-                
                 if c_sqrt.is_integer() and c_sqrt <= n:
                     ans += 1
+
         return ans
