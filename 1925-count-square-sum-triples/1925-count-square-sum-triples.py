c84c508471ef490eb8be01039c2251b08c5b47cc
Bartosz Malczyk <142089705+bmalczan@users.noreply.github.com>
2025-12-08 15:58:21 +0100

    Time: 207 ms (50%), Space: 18 MB (11.68%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        ans = 0 
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                val = i ** 2 + j ** 2
+                c_sqrt = math.sqrt(val)
+                
+                if c_sqrt.is_integer() and c_sqrt <= n:
+                    ans += 1
+        return ans
