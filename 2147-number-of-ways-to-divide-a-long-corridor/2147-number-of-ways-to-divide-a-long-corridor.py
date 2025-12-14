4e36713d3f8b6bbc2a921063f91545a21a4f6e1e
Bartosz Malczyk <142089705+bmalczan@users.noreply.github.com>
2025-12-14 12:29:34 +0100

    Time: 329 ms (42.86%), Space: 22.6 MB (27.62%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,5 +1,3 @@
-import math
-
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
         seats = []
@@ -9,14 +7,10 @@ class Solution:
         seats_number = len(seats)
         if seats_number % 2 != 0 or seats_number == 0:
             return 0
-
-        sections = []
         spaces = 1
         for j in range(0,len(seats) - 1):
             if j % 2 != 0:
-                end = seats[j]
-                start = seats[j+1]
-                spaces *= (start - end)
+                spaces *= (seats[j+1] - seats[j])
         return spaces % (10**9 + 7)
 
             
