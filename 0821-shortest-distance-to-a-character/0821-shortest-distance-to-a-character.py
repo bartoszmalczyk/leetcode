9715039ebbff017b2baf8ba9d7f98ea003553e52
Bartosz Malczyk <142089705+bmalczan@users.noreply.github.com>
2025-12-25 12:33:27 +0100

    Time: 4 ms (40.81%), Space: 17.4 MB (93.36%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -3,15 +3,17 @@ class Solution:
         n = len(s)
         ans = [n] * n 
         
-
-        prev = -n 
+        prev = -n
         for i in range(n):
             if s[i] == c:
-                prev = i
-            ans[i] = i - prev 
-        prev = 2 * n 
-        for i in range(n - 1, -1, -1): 
+                prev = i 
+            ans[i] = i - prev
+        
+        prev = 2*n
+        for i in range(n - 1, -1, -1):
             if s[i] == c:
                 prev = i
             ans[i] = min(ans[i], prev - i)
         return ans
+
+        
