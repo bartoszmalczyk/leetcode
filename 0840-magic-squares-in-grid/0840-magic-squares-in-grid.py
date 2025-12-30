3c13f01bdf0469332944548f87f8ab38abbdb954
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2025-12-30 13:39:58 +0100

    Time: 3 ms (44.24%), Space: 17.2 MB (97.58%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,30 +1,37 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        def isMagic(r,c): #starting at (r,c), ending at (r+2,c+2)
-            temp = set()
-            req = sum(grid[r][c:c+3])
-            for i in range(r,r+3):
-                row_sum = 0
-                col_sum = 0
-                for j in range(c,c+3):
-                    #unique
-                    if grid[i][j] in temp:
+        def isMagic(r, c):
+            #check if unique and < 10
+            seen = set()
+            for i in range(r, r + 3):
+                for j in range(c, c + 3):
+                    val = grid[i][j]
+                    if val < 1 or val > 9 or val in seen:
                         return False
-                    else:
-                        temp.add(grid[i][j])
-                    #horizontal & verical 
-                    row_sum += grid[i][j]
-                    col_sum += grid[j][i]
-                if row_sum != req or col_sum != req: return False
-            #diagonal 
-            first_diagonal = grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2]
-            second_diagonal = grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c]
-            if first_diagonal != req or second_diagonal != req: return False
+                    seen.add(val)
+            
+            #rows sum check
+            for i in range(r, r + 3):
+                if sum(grid[i][c:c+3]) != 15:
+                    return False
+            
+            # cols sum check
+            for j in range(c, c + 3):
+                if grid[r][j] + grid[r+1][j] + grid[r+2][j] != 15:
+                    return False
+            
+            # diagonals
+            diag1 = grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2]
+            diag2 = grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c]
+            
+            if diag1 != 15 or diag2 != 15:
+                return False
             return True
-        ans = 0 
-        for i in range(len(grid) - 2):
-            for j in range(len(grid[0]) - 2):
-                if isMagic(i,j): ans += 1
-        return ans
 
-                
+        rows, cols = len(grid), len(grid[0])
+        ans = 0
+        for i in range(rows - 2):
+            for j in range(cols - 2):  
+                if isMagic(i, j):
+                    ans += 1
+        return ans
