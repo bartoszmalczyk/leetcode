4121ff3e2a31dbd807aa80fad6afe8f6eac21a29
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-01-02 01:14:26 +0100

    Time: 0 ms (100%), Space: 18.4 MB (95.44%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -1,21 +1,8 @@
+from collections import defaultdict
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        candidate1, count1 = None, 0
-        candidate2, count2 = None, 0
-
+        hm = defaultdict(int)
         for num in nums:
-            if candidate1 == num:
-                count1 += 1
-            elif candidate2 == num:
-                count2 += 1
-            elif count1 == 0:
-                candidate1, count1 = num, 1
-            elif count2 == 0:
-                candidate2, count2 = num, 1
-            else:
-                count1 -= 1
-                count2 -= 1            
-        if nums.count(candidate1) >= nums.count(candidate2):
-            return candidate1
-        else:
-            return candidate2
+            hm[num] += 1
+            if hm[num] >= 2:
+                return num
