1266329416be1393b88454daba932ff04a7808d4
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-02-02 14:13:21 +0100

    Time: 1 ms (64.6%), Space: 19.3 MB (34.55%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,12 @@
+from collections import deque 
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        res = [p for p in prices]
+        stack = deque([]) #indexes 
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                j = stack.pop() 
+                res[j] -= prices[i] 
+            stack.append(i)
+        return res
