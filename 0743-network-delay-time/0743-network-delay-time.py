635a2ae0efe673d6b695072ddd4aa98d198c24e1
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-02-07 17:24:10 +0100

    Time: 348 ms (64.15%), Space: 22 MB (7.41%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -8,13 +8,13 @@ class Solution:
         shortest = {}
         minHeap = [[0, k]]
         while minHeap:
-            n1, w1 = heapq.heappop(minHeap)
+            w1, n1 = heapq.heappop(minHeap)
             if n1 in shortest:
                 continue
             shortest[n1] = w1
             for n2, w2 in adj[n1]:
                 if n2 not in shortest:
-                    heapq.heappush(minHeap, [n2, w1 + w2])
+                    heapq.heappush(minHeap, [w1 + w2, n2])
         ans = max(shortest.values())
         if len(shortest) != n: return -1
         return ans
