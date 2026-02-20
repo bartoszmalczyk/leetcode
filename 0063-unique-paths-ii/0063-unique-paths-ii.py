30ba8eee0aea007308b5355172abdd42fa2dc80f
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-02-21 00:44:46 +0100

    Time: 0 ms (100%), Space: 19.6 MB (14.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,23 +1,24 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
+            
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        dp = [[0 for i in range(n)] for j in range(m)]
+        
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
         for r in range(m):
             for c in range(n):
                 if obstacleGrid[r][c] == 1: 
                     dp[r][c] = 0
-                    continue
-                else:
-                    if r == 0 and c == 0:
-                        if obstacleGrid[r][c] == 1: 
-                            return 0
-                        dp[r][c] = 1
-                    elif r < 1:
-                        dp[r][c] += dp[r][c - 1]
-                    elif c < 1:
-                        dp[r][c] += dp[r - 1][c]
-                    else:
-                        dp[r][c] += dp[r - 1][c]
-                        dp[r][c] += dp[r][c - 1]
+                elif r == 0 and c == 0:
+                    dp[r][c] = 1
+                elif r == 0:  
+                    dp[r][c] = dp[r][c - 1]
+                elif c == 0:  
+                    dp[r][c] = dp[r - 1][c]
+                else:        
+                    dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+                    
         return dp[m - 1][n - 1]
