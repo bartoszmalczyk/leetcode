064c1009c4cf60a6a624e31e07891cc49f0b2ddd
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-03-08 10:50:01 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -4,15 +4,19 @@ class Solution:
         n = len(nums[0])
         for bin_str in nums:
             nums_set.add(int(bin_str, 2))
+        
         if 0 not in nums_set:
             return "0" * n
+
         for i in range(min(nums_set), 2**16):
             if i not in nums_set:
                 sol = format(i, 'b')
                 break
+        return "0" * (n - len(sol)) + sol
         ans = ""
         for i in range(n - len(sol)):
             ans += "0"
         ans += sol
+
         return ans
             
