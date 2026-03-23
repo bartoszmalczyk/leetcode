4db78e77b02ac43072228fe9d29bb5dedb8aeaf5
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-03-23 17:49:33 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        float waiting = 0;
+        int last_finish = customers[0][0];
+        for (int i = 0; i < customers.size(); i++){
+            int extra_time = 0;
+            int arrival = customers[i][0];
+            int time = customers[i][1];
+
+            if (last_finish <= arrival){
+                waiting += time;
+                last_finish = arrival + time;
+            }
+            else{
+                extra_time = last_finish - arrival;
+                waiting += time + extra_time;
+                last_finish += time;
+            }
+        }
+        return waiting / customers.size();    
+    }
+};
