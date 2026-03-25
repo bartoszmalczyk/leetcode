314908742cd1c863b14012048894c438c836b871
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-03-25 14:56:23 +0100

    Time: 114 ms (82.19%), Space: 56.2 MB (5.48%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -3,14 +3,34 @@ from typing import List
 
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        m = len(grid)
-        n = len(grid[0])
-        grid = np.array(grid) 
+        m, n = len(grid), len(grid[0])
+        sum_ = sum(grid[0])
+
+        pre_rows = [sum(grid[0])]
+        for i in range(1,m):
+            temp = sum(grid[i])
+            pre_rows.append(pre_rows[- 1] + temp)
+            sum_ += temp
+        pre_cols = []
+
+        if sum_ % 2 != 0:
+            return False
+
+        for row in pre_rows[:-1]:
+            if row == sum_ - row: return True
+
+        pre_col = [0]
+        for i in range(m):
+            pre_col[0] += grid[i][0]
         
-        for i in range(0, m):
-            if np.sum(grid[:i, :]) == np.sum(grid[i:, :]):
-                return True
-        for j in range(0, n):
-            if np.sum(grid[:, :j]) == np.sum(grid[:, j:]):
-                return True
+        for i in range(1, n):
+            temp = 0
+            for j in range(m):
+                temp += grid[j][i]
+            pre_col.append(pre_col[-1] + temp)
+
+        for col in pre_col:
+            if col == sum_ - col: return True
         return False
+
+
