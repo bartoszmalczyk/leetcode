a08c66b2293aa8ee2eaff65341cab3c89def90e5
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-03-30 14:34:45 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int max_ = releaseTimes[0];
+        int index = 0;
+        for (int i = 1; i < releaseTimes.size(); i++){
+            int temp = releaseTimes[i] - releaseTimes[i-1];
+            if (temp > max_){
+                max_ = temp;
+                index = i;
+            }
+            else if (temp == max){
+                if (keysPressed[i] > keysPressed[index]){
+                    index = i;
+                }
+            }
+        }
+        return keysPressed[index];
+    }
+};
