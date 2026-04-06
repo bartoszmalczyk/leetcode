e59c006dafb4eef9b0461b61429f21caa39a089e
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-04-06 22:53:27 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.py
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -1,12 +1,6 @@
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
-        directions = ['u','r','d','l']
-        dir_map = {
-            'u' : (0,1),
-            'd' : (0,-1),
-            'l' : (-1,0), 
-            'r' : (1,0)}
-
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         curr_direction = 0
         max_distance = 0
         obstacles_set = set(tuple(obstacle) for obstacle in obstacles)
@@ -18,7 +12,6 @@ class Solution:
                 curr_direction = (curr_direction - 1) % 4
             else:
                 for _ in range(command):
-                    next_pos = (position[0] + dir_map[directions[curr_direction]][0], position[1] + dir_map[directions[curr_direction]][1])
                     if next_pos not in obstacles_set:
                         position = next_pos
                     else:
