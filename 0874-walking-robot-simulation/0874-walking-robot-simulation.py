443a8c094aa0381b2075a53e035acac8f1e0fbb1
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-04-06 22:54:21 +0200

    Time: 23 ms (96.4%), Space: 23.7 MB (93.24%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.py
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -6,17 +6,19 @@ class Solution:
         obstacles_set = set(tuple(obstacle) for obstacle in obstacles)
         position = [0,0]
         for command in commands:
-            if command == -1:
-                curr_direction = (curr_direction + 1) % 4
-            elif command == -2:
-                curr_direction = (curr_direction - 1) % 4
+            temp = 0
+            if command == -1: temp = 1
+            elif command == -2: temp = -1
             else:
                 for _ in range(command):
+                    dx, dy = directions[curr_direction]
+                    next_pos = (position[0] + dx, position[1] + dy)
                     if next_pos not in obstacles_set:
                         position = next_pos
                     else:
                         break
                 max_distance = max(max_distance, position[0]**2 + position[1]**2)
+            curr_direction = (curr_direction + temp) % 4
         return max_distance
 
 
