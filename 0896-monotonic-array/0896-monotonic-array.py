8702b41905f48350e6cc671ff54cc2c64a12b085
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-04-08 07:21:10 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -3,10 +3,6 @@ class Solution:
         if len(nums) == 1: return True
         increasing = (nums[0] < nums[-1])
         for i in range(0,len(nums) - 1):
-            if increasing: 
-                if nums[i+1] < nums[i]:
-                    return False
-            else:
-                if nums[i+1] > nums[i]:
-                    return False
+            if (increasing and nums[i+1] < nums[i]) or (not increasing and nums[i+1] > nums[i]): 
+                return False
         return True
