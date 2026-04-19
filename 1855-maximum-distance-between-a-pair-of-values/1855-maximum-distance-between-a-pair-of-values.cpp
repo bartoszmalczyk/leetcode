0a26a4051e2b6da3aa4a78e3aaac25b74494ab2c
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-04-19 23:45:50 +0200

    Time: 19 ms (17.52%), Space: 102.2 MB (75.16%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,19 +1,23 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+    int maxDistance(std::vector<int>& nums1, std::vector<int>& nums2) {
         int ans = 0;
-        int i = 0;
-        int j = 0;
-        int mid = 0;
-        for (int index = 0; index < nums2.size(); index++){
-            i = 0;
-            j = std::min(index, nums1.size() - 1)
-            while (i <= j){
-                mid = (i + j) / 2;
-                if (nums1[mid] > nums2[index]){
+        
+        // Let's use int for the size to avoid type mismatch issues later
+        int n1 = nums1.size(); 
+        
+        for (int index = 0; index < nums2.size(); index++) {
+            int i = 0;
+            int j = std::min(index, n1 - 1); 
+            while (i <= j) {
+                int mid = i + (j - i) / 2; 
+                
+                if (nums1[mid] > nums2[index]) {
                     i = mid + 1;
-                }
-                else{
+                } else {
                     ans = std::max(ans, index - mid);
                     j = mid - 1;
                 }
