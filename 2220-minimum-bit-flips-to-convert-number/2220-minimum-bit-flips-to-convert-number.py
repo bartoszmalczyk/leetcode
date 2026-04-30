91e85d5e39468332530fd23898f6b8417e421fd5
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-04-30 17:12:59 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,7 +1,9 @@
 import math
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        # 10 1010
-        # 7  0111
         ans = start ^ goal 
         return ans.bit_count()
+
+        # 
+        # 
+        /
