3a2c8381e19385eb12088db205a53d2477f864e3
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-05-05 17:47:17 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,21 +4,11 @@ class Solution:
         pre = [1] * n
         sub = [1] * n 
 
-        # pre populate 
+        # pre & sub populate 
         for i in range(1,n):
             pre[i] = pre[i - 1] * nums[i - 1]
-        
-        # sub populate 
-        for j in range(n - 2, -1, -1):
-            sub[j] = sub[j + 1] * nums[j + 1]
+            sub[n - 1 - i] = sub[n - i] * nums[n - i]
         
         ans = [pre[k] * sub[k] for k in range(n)]
         return ans
 
-
-"""
-nums [1,2,3,4]
-pre = [1, 1, 2, 6]
-sub = [24, 12, 4, 1]
-
-"""
