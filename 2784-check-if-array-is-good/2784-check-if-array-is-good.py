bed3324e35bf9ca86cd57cc0946c70caeefa6139
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-05-14 14:08:03 +0200

    Time: 3 ms (29.89%), Space: 19.4 MB (3.2%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,11 +1,10 @@
-from collections import defaultdict 
+from collections import Counter 
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
         ones_counter = 0 
         two_counter = 0
-        hm = defaultdict(int)
-        for num in nums:
-            hm[num] += 1
+        hm = Counter(nums)
+        
         for i in range(1, len(nums) - 1):
             if hm[i] != 1:
                 return False
