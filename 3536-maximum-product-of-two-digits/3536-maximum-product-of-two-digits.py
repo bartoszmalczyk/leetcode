61264027b19f42938803188401be794fff9ea503
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-07-25 13:16:18 +0200

    Time: 0 ms (100%), Space: 19.6 MB (22.61%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -1,10 +1,14 @@
 import heapq
+
 class Solution:
     def maxProduct(self, n: int) -> int:
-        max_heap = []
-        while n >= 1:
-            heapq.heappush(max_heap, -1 * (n % 10))
+        digits = []
+        while n > 0:
+            digits.append(-(n % 10))
             n //= 10
-        a = heappop(max_heap) * -1
-        b = heappop(max_heap) * -1
+            
+        heapq.heapify(digits)
+        a = -heapq.heappop(digits)
+        b = -heapq.heappop(digits)
+        
         return a * b
