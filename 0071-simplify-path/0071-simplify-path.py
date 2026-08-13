82ce474f7568e4e2e2a9f7f07f99417048f38f40
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-08-13 14:33:07 +0200

    Time: 0 ms (100%), Space: 19.4 MB (23.61%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,32 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        dot_counter = 0
+        temp = path.split("/")
+        print(stack)
+
+        stack = []
+        for name in temp:
+            if not name or name == ".":   
+                continue
+            if name == "..":
+                if stack:
+                    stack.pop()
+                continue
+            else:
+                stack.append(name)
+        ans = "/"
+        do_flash = False
+        for name in stack:
+            if do_flash:
+                ans += (f"/{name}")
+            else:
+                ans += (f"{name}")
+                do_flash = True
+        return ans
+            
+            
+
+
+
+                    
