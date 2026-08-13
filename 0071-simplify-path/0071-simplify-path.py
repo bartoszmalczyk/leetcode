7f7abd33924853f8adaffb637d12654ecf883c8f
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-08-13 14:35:29 +0200

    Time: 0 ms (100%), Space: 19.4 MB (23.61%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -10,16 +10,8 @@ class Solution:
             if name == "..":
                 if stack:
                     stack.pop()
-                continue
             else:
                 stack.append(name)
-        ans = "/"
-        do_flash = False
-        for name in stack:
-            if do_flash:
-                ans += (f"/{name}")
-            else:
-                ans += (f"{name}")
-                do_flash = True
-        return ans
+        return "/" + "/".join(stack)
+    
             
