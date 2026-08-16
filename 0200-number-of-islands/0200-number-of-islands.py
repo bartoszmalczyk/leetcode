f3d9eedf1c7f5605b97aa3c312ad4aa28cfe517a
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-08-16 12:08:04 +0200

    Time: 273 ms (15.47%), Space: 21.6 MB (72.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
         directions = {(0,1), (0,-1), (1,0), (-1,0)}
         m = len(grid)
         n = len(grid[0])
         counter = 0
         def is_valid(r,c):
-            return True if (
-                r >= 0 and r < m and
-                c >= 0 and c < n
-                ) else False
+            return 0 <= r < m and 0 <= c < n
 
         def dfs(r,c):
-            if not is_valid(r,c) or grid[r][c] == "0" or (r,c) in visited:
+            if not is_valid(r,c) or grid[r][c] == "0":
                 return
-            visited.add((r,c))
+            grid[r][c] = "0"
             for rr,cc in directions:
                 dfs(r + rr,c + cc)
 
         for r in range(m):
             for c in range(n):
-                if (r,c) not in visited and grid[r][c] == "1":
+                if grid[r][c] == "1":
                     dfs(r,c)
                     counter += 1
         return counter
