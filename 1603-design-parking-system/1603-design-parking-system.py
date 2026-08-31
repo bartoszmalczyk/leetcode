b336e080881cb47548f20e569897e44325890178
Bartosz Malczyk <142089705+bartoszmalczyk@users.noreply.github.com>
2026-08-31 19:24:17 +0200

    Time: 9 ms (79.28%), Space: 19.7 MB (90.3%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,23 +1,11 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small        
-
+        self.sizes = [big, medium, small]      
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big:
-                self.big -= 1
-                return True
-        elif carType == 2:
-            if self.medium:
-                self.medium -= 1 
-                return True
-        elif carType == 3:
-            if self.small:
-                self.small -= 1
-                return True
+        if self.sizes[carType - 1]:
+            self.sizes[carType - 1] -= 1
+            return True
         return False
 
 
